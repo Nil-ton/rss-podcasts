61e0b790e7d221ef11062a645ad824bc52df3c29
Nil-ton <nilton.oliveira0305@gmail.com>
2024-08-21 13:44:58 -0300

    [200~

--- renderer/components/Main/MainRoot.tsx
+++ renderer/components/Main/MainRoot.tsx
@@ -1,10 +1,14 @@
+import { stateManager } from "../../stateManager"
+
 interface IMainRoot {
     children: React.ReactNode
 }
 
 export function MainRoot({children}: IMainRoot) {
+    const selectedPodcast = stateManager.useSelectedPodcastPlay((state) => state.item)
+ 
     return (
-        <div className="p-2 m-2 rounded-xl bg-background-base shadow-2xl pb-32 h-screen overflow-auto">
+        <div style={{height: selectedPodcast ? '80vh' : '100vh' }} className="p-2 m-2 rounded-xl bg-background-base shadow-2xl overflow-auto">
             {children}
         </div>
     )
