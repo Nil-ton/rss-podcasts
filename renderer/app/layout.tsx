eafde8f0ac9accf10e339c39fe745b768b37b45d
Nil-ton <nilton.oliveira0305@gmail.com>
2024-08-29 14:29:03 -0300

    feat: adicionar o header system no layout
    esqueci de adicionar o header no layout. Isso gera bugs visuais que será
    corrigido no proximo commit

--- renderer/app/layout.tsx
+++ renderer/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Footer } from '../components/Footer'
+import { HeaderSystem } from '../components/HeaderSystem'
 import { Dialog } from '../components/Modal'
 import { Player } from '../components/Player'
 import { Sidebar } from '../components/Siderbar'
@@ -10,34 +11,42 @@ export default function RootLayout({
 }) {
     return (
         <html lang='pt-br'>
-            <body className='flex gap-1 fixed min-h-screen z-0'>
-                <Dialog.Root>
-                    <Dialog.Content>
-                        <Dialog.Title text='Adicionar' />
-                        <Dialog.Subtitle text='Insira o URL RSS do podcast que você deseja adicionar à lista. Você pode encontrar RSS de podcasts em https://chartable.com/.' />
-                        <Dialog.Form />
-                    </Dialog.Content>
-                </Dialog.Root>
+            <body>
+                <HeaderSystem.Root>
+                    <HeaderSystem.Minimize />
+                    <HeaderSystem.Maximize />
+                    <HeaderSystem.Close />
+                </HeaderSystem.Root>
 
-                <Sidebar.Root>
-                    <Sidebar.Content>
-                        <Sidebar.Header />
-                        <Sidebar.ActionsAdd />
-                    </Sidebar.Content>
-                    <Sidebar.Content>
-                        <Sidebar.Library />
-                    </Sidebar.Content>
-                </Sidebar.Root>
+                <div className='flex gap-1 fixed min-h-screen z-0'>
+                    <Dialog.Root>
+                        <Dialog.Content>
+                            <Dialog.Title text='Adicionar' />
+                            <Dialog.Subtitle text='Insira o URL RSS do podcast que você deseja adicionar à lista. Você pode encontrar RSS de podcasts em https://chartable.com/.' />
+                            <Dialog.Form />
+                        </Dialog.Content>
+                    </Dialog.Root>
 
-                {children}
+                    <Sidebar.Root>
+                        <Sidebar.Content>
+                            <Sidebar.Header />
+                            <Sidebar.ActionsAdd />
+                        </Sidebar.Content>
+                        <Sidebar.Content>
+                            <Sidebar.Library />
+                        </Sidebar.Content>
+                    </Sidebar.Root>
 
-                <Footer.Root>
-                    <Player.Root>
-                        <Player.Info />
-                        <Player.Progress />
-                        <Player.Control />
-                    </Player.Root>
-                </Footer.Root>
+                    {children}
+
+                    <Footer.Root>
+                        <Player.Root>
+                            <Player.Info />
+                            <Player.Progress />
+                            <Player.Control />
+                        </Player.Root>
+                    </Footer.Root>
+                </div>
             </body>
         </html>
     )
