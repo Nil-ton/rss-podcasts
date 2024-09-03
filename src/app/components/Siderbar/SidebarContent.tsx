9c4be3453c0b022295dba1d15206b57ad6fb9b7c
Nil-ton <nilton.oliveira0305@gmail.com>
2024-09-03 17:44:25 -0300

    feat: adicionar o data-test em SidebarContent
    adicionei o apropriedade data-test em sidebar content para fazer testes
    e2e

--- src/app/components/Siderbar/SidebarContent.tsx
+++ src/app/components/Siderbar/SidebarContent.tsx
@@ -5,7 +5,7 @@ interface ISiderbarContent {
 }
 export function SidebarContent({children, height='max-h-full', overflow = 'auto'}: ISiderbarContent) {
     return (
-        <div className={`w-full ${height} overflow-${overflow} rounded-xl bg-background-base p-2 flex flex-col gap-5 shadow-xl`}>
+        <div className={`w-full ${height} overflow-${overflow} rounded-xl bg-background-base p-2 flex flex-col gap-5 shadow-xl`} data-test="sidebar-content">
             {children}
         </div>
     )
